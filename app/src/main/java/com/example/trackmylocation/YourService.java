56a647ec5934b6acf2d5039956abb62e853b0891
vishnu-hub <vishnupnambiar1999@gmail.com>
2020-04-10 21:06:40 +0530

    Location

---
+++ app/src/main/java/com/example/trackmylocation/YourService.java
@@ -0,0 +1,47 @@
+package com.example.trackmylocation;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+public class YourService extends Service {
+
+    private static final int NOTIF_ID = 1;
+    private static final String NOTIF_CHANNEL_ID = "Channel_Id";
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+
+        // do your jobs here
+
+        startForeground();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void startForeground() {
+        Intent notificationIntent = new Intent(this, MapsActivity.class);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                notificationIntent, 0);
+
+        startForeground(NOTIF_ID, new NotificationCompat.Builder(this,
+                NOTIF_CHANNEL_ID) // don't forget create a notification channel first
+                .setOngoing(true)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText("Service is running background")
+                .setContentIntent(pendingIntent)
+                .build());
+    }
+}
